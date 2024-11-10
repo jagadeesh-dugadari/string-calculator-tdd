0accc19b0f53784c0be2aef9ef0eb78c4f5a10c6
jagadeesh-dugadari <man.jagadeesh@gmail.com>
2024-11-10 21:32:13 +0530

    Refactored code

--- src/main/java/co/incubyte/stringcalculator/StringCalculator.java
+++ src/main/java/co/incubyte/stringcalculator/StringCalculator.java
@@ -11,6 +11,8 @@ public class StringCalculator {
 	private static final String COMMA_STRING = ",";
 	private static final String NEW_LINE_STRING = "\n";
 	private static final String COMMA_OR_NEW_LINE_STRING = ",|\n";
+	private static final String DOUBLE_SLASH_STRING = "//";
+	private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
 	
 	
 	public static int add(String numbers) {
@@ -23,18 +25,22 @@ public class StringCalculator {
 				result = 0;
 			} else if (trimmedInput.matches(NUMBER_STRING)) {
 				result = Integer.valueOf(trimmedInput);
-			} else if (trimmedInput.startsWith("//")) {
-				Matcher m = Pattern.compile("//(.)\n(.*)").matcher(numbers);
-				if (m.matches()) {
-					String customDelim = m.group(1);
-					String nums = m.group(2);
-					result = Arrays.stream(nums.split(Pattern.quote(customDelim))).mapToInt(num -> Integer.valueOf(num.trim())).sum();
+			} else if (trimmedInput.startsWith(DOUBLE_SLASH_STRING)) {
+				Matcher customDelimiterMatcher = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(numbers);
+				if (customDelimiterMatcher.matches()) {
+					String customDelimiter = customDelimiterMatcher.group(1);
+					String numsWithCustomDelimiter = customDelimiterMatcher.group(2);
+					result = getSumForStringArray(numsWithCustomDelimiter.split(Pattern.quote(customDelimiter)));
 				}
 			} else if (trimmedInput.contains(COMMA_STRING) || trimmedInput.contains(NEW_LINE_STRING)) {
-				result = Arrays.stream(trimmedInput.split(COMMA_OR_NEW_LINE_STRING)).mapToInt(num -> Integer.valueOf(num.trim())).sum();
+				result = getSumForStringArray(trimmedInput.split(COMMA_OR_NEW_LINE_STRING));
 			}
 		}
 		
 		return result;
+	}
+	
+	public static int getSumForStringArray(String[] arr) {
+		return Arrays.stream(arr).mapToInt(num -> Integer.valueOf(num.trim())).sum();
 	}
 }
