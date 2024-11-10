9762cae515b9152049c7b1b10f6fe23df8e7695e
jagadeesh-dugadari <man.jagadeesh@gmail.com>
2024-11-10 19:16:20 +0530

    Added logic to pass first test case

--- src/main/java/co/incubyte/stringcalculator/StringCalculator.java
+++ src/main/java/co/incubyte/stringcalculator/StringCalculator.java
@@ -1,8 +1,12 @@
 package co.incubyte.stringcalculator;
 
 public class StringCalculator {
-
+	
 	public static int add(String numbers) {
+		
+		if (numbers.equals("")) {
+			return 0;
+		}
 		
 		return -1;
 	}
