1b0ee79d6e83d8fc612ab6f8c4f98b02f3ac9144
jagadeesh-dugadari <man.jagadeesh@gmail.com>
2024-11-10 21:00:27 +0530

    Refactored code

--- src/main/java/co/incubyte/stringcalculator/StringCalculator.java
+++ src/main/java/co/incubyte/stringcalculator/StringCalculator.java
@@ -7,6 +7,9 @@ public class StringCalculator {
 	private static final String EMPTY_STRING = "";
 	private static final String NUMBER_STRING = "\\d+";
 	private static final String COMMA_STRING = ",";
+	private static final String NEW_LINE_STRING = "\n";
+	private static final String COMMA_OR_NEW_LINE_STRING = ",|\n";
+	
 	
 	public static int add(String numbers) {
 		
@@ -18,8 +21,8 @@ public class StringCalculator {
 				result = 0;
 			} else if (trimmedInput.matches(NUMBER_STRING)) {
 				result = Integer.valueOf(trimmedInput);
-			} else if (trimmedInput.contains(COMMA_STRING) || trimmedInput.contains("\n")) {
-				result = Arrays.stream(trimmedInput.split(",|\n")).mapToInt(num -> Integer.valueOf(num.trim())).sum();
+			} else if (trimmedInput.contains(COMMA_STRING) || trimmedInput.contains(NEW_LINE_STRING)) {
+				result = Arrays.stream(trimmedInput.split(COMMA_OR_NEW_LINE_STRING)).mapToInt(num -> Integer.valueOf(num.trim())).sum();
 			}
 		}
 		
