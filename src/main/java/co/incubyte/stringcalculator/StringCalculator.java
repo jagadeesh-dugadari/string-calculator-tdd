a1f37ef8419b62c9cd3bb73eb6cda38428c850c9
jagadeesh-dugadari <man.jagadeesh@gmail.com>
2024-11-10 21:20:44 +0530

    Added logic to pass tenth test case

--- src/main/java/co/incubyte/stringcalculator/StringCalculator.java
+++ src/main/java/co/incubyte/stringcalculator/StringCalculator.java
@@ -1,6 +1,8 @@
 package co.incubyte.stringcalculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 	
@@ -21,6 +23,13 @@ public class StringCalculator {
 				result = 0;
 			} else if (trimmedInput.matches(NUMBER_STRING)) {
 				result = Integer.valueOf(trimmedInput);
+			} else if (trimmedInput.startsWith("//")) {
+				Matcher m = Pattern.compile("//(.)\n(.*)").matcher(numbers);
+				if (m.matches()) {
+					String customDelim = m.group(1);
+					String nums = m.group(2);
+					result = Arrays.stream(nums.split(Pattern.quote(customDelim))).mapToInt(num -> Integer.valueOf(num.trim())).sum();
+				}
 			} else if (trimmedInput.contains(COMMA_STRING) || trimmedInput.contains(NEW_LINE_STRING)) {
 				result = Arrays.stream(trimmedInput.split(COMMA_OR_NEW_LINE_STRING)).mapToInt(num -> Integer.valueOf(num.trim())).sum();
 			}
