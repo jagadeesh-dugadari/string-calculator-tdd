25d7421a9a70bbd772dfd3e04eff7abed33e170d
jagadeesh-dugadari <man.jagadeesh@gmail.com>
2024-11-10 18:54:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Assignment for [Incubyte](https://incubyte.co)
+
+String Calculator TDD Kata described [here](https://blog.incubyte.co/blog/tdd-assessment/) coded in Java
---
+++ src/test/java/co/incubyte/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package co.incubyte.stringcalculator;
+
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+	
+	@Test
+	public void nothing() {
+		
+	}
+
+}
